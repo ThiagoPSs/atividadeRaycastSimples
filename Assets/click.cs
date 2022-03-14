3f3fe26df8de54037dabd7f4b01e2e47027a3f78
ThiagoPSs <psthigo@gmail.com>
2022-03-14 17:39:46 -0300

    Inclusao dos arquivos

---
+++ Assets/click.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour
+{
+    RaycastHit hit;
+    public Transform alvo;
+    Renderer rend;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit, 100))
+            {
+                alvo.position = hit.point;
+               
+            }
+        }
+    }
+}
